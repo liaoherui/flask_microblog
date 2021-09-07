f7cf823a2f42501e68fcac14e4f7a160c5b60698
liaoherui <851836818@qq.com>
2021-09-07 12:47:14 +0800

    chapter03 forms finished

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from config import Config
 
 app=Flask(__name__)
+app.config.from_object(Config)
 
-print('Who use me: ',__name__)
+#print('Who use me: ',__name__)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template,flash,redirect
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +17,11 @@ def index():
 			}
 	]
 	return render_template("index.html",title='Home',user=user,posts=posts)
+
+@app.route('/login',methods=["GET","POST"])
+def login():
+	login_form=LoginForm()
+	if login_form.validate_on_submit():
+		flash('Login requested for user {}, remember_me={}'.format(login_form.username.data,login_form.remember_me.data))
+		return redirect(url_for('index'))
+	return render_template('login.html',title='Sign In',form=login_form)
