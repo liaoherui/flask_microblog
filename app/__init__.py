c7657771e98101eeb086e6d3d6713b1d495566e3
liaoherui <851836818@qq.com>
2021-09-07 14:13:10 +0800

    chapter04 database finished

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from config import Config
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
 app=Flask(__name__)
 app.config.from_object(Config)
 
 #print('Who use me: ',__name__)
 
-from app import routes
+db=SQLAlchemy(app)
+migrate=Migrate(app,db)
+
+from app import routes,models
