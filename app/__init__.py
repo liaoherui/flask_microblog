c925aa3c0b48b34de6bb62dfe7d7305de4bfc271
liaoherui <851836818@qq.com>
2021-09-08 12:46:48 +0800

    chapter05 user_login_reg finished

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,19 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from flask_login import LoginManager
+
 
 app=Flask(__name__)
 app.config.from_object(Config)
+
 
 #print('Who use me: ',__name__)
 
 db=SQLAlchemy(app)
 migrate=Migrate(app,db)
+
+login = LoginManager(app)
+login.login_view='login'
 
 from app import routes,models
