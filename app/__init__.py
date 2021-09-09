352f4c047433171c3b4e944009ed2d3adc91b7f9
liaoherui <851836818@qq.com>
2021-09-09 20:36:14 +0800

    chapter11 flask_moment finished

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,8 @@ from flask_mail import Mail
 
 from flask_bootstrap import Bootstrap
 
+from flask_moment import Moment
+
 
 app=Flask(__name__)
 app.config.from_object(Config)
@@ -24,5 +26,7 @@ login.login_view='login'
 
 mail=Mail(app)
 bootstrap=Bootstrap(app)
+
+moment=Moment(app)
 
 from app import routes,models
